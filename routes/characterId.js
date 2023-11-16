970a0bd117e46f3619b39969e4e890228fb2d090
Joanan Boureille <joananboureille@ordinatdejoanan.lan>
2023-11-16 17:11:16 +0100

    All three routes

--- routes/characterId.js
+++ routes/characterId.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/comics/:characterId", async (req, res) => {
-  const response = await axios.get(
-    `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}`
-  );
-  console.log(response.data);
-  res.json(response.data);
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}`
+    );
+
+    console.log("hey");
+    res.json(response.data);
+  } catch (error) {
+    console.log("erreur =>", error);
+  }
+
+  // console.log(response.data);
+  // res.json(response.data);
 });
 module.exports = router;
